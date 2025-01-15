9e7536a181f3989d5efba7d1c9e33b8c93706243
Jake D <jmdonovan0227@gmail.com>
2025-01-14 21:18:36 -0500

    Added error logs to check env vars being passed to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import { isCompositeComponent } from 'react-dom/test-utils';
 export default defineConfig(({ mode }) => {
   console.log('mode: ', mode);
   const env = loadEnv(mode, process.cwd());
   console.log('env: ', env);
-  console.log(import.meta.env.VITE_APP_URL);
+  // console.log(import.meta.env.VITE_APP_URL);
+  console.log(env.VITE_APP_URL);
   console.log(process.env.VITE_APP_URL);
 
   return {
